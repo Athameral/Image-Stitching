bc7d4510426767d82a2099cdbecde0479e19e384
Athameral <Athameral@outlook.com>
2024-06-18 15:50:59 +0800

    Reconstruct and move many functions to cvoperations.

--- cvoperations.py
+++ cvoperations.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 
 def center_images(
@@ -14,9 +15,102 @@ def center_images(
             y_offset : y_offset + IMAGE_HEIGHT, x_offset : x_offset + IMAGE_WIDTH, :
         ] = imgs[img_idx]
         imgs[img_idx] = canvas.copy()
-        cv2.imshow(f"{img_idx=}", cv2.resize(imgs[img_idx], (1500, 1000)))
-        cv2.waitKey(0)
-        cv2.destroyWindow(f"{img_idx=}")
+        # cv2.imshow(f"{img_idx=}", cv2.resize(imgs[img_idx], (1500, 1000)))
+        # cv2.waitKey(0)
+        # cv2.destroyWindow(f"{img_idx=}")
 
 
+def get_kps_descs_dict(imgs: dict[int, cv2.typing.MatLike], detector: cv2.SIFT):
+    kps_descs_dict: dict[int, tuple[tuple[cv2.KeyPoint], np.ndarray]] = {}
+    for img_idx, img in imgs.items():
+        if img_idx not in kps_descs_dict:
+            kps_descs_dict[img_idx] = detector.detectAndCompute(img, None)
+    return kps_descs_dict
 
+
+def get_matches_dict(
+    chains: dict[int, list[tuple[int, int]]],
+    kps_descs_dict: dict[int, tuple[tuple[cv2.KeyPoint], np.ndarray]],
+    matcher: cv2.BFMatcher,
+):
+    matches_dict: dict[tuple[int, int], tuple[cv2.DMatch]] = {}
+    for chain in chains.values():
+        for junction in chain:
+            if junction not in matches_dict:
+                src_idx, dst_idx = junction
+                # Note that, we try to match the descriptors here, i.e.,
+                # calculate the distance between every 2 descriptors.
+                matches_dict[junction] = matcher.match(
+                    # Take especial care for this, query is the source,
+                    # whereas train is the destination.
+                    kps_descs_dict[src_idx][1],
+                    kps_descs_dict[dst_idx][1],
+                )
+    # out = np.zeros((IMAGE_HEIGHT, IMAGE_WIDTH * 2, 3))
+    # kps1 = kps_descs_dict[src_idx][0]
+    # kps2 = kps_descs_dict[dst_idx][0]
+    # matches = matches_dict[junction]
+    # out = cv2.drawMatches(
+    #     imgs[src_idx], kps1, imgs[dst_idx], kps2, matches, out, 10
+    # )
+    return matches_dict
+
+
+def get_warps_dict(
+    chains: dict[int, list[tuple[int, int]]],
+    kps_descs_dict: dict[int, tuple[tuple[cv2.KeyPoint], np.ndarray]],
+    matches_dict: dict[tuple[int, int], tuple[cv2.DMatch]],
+):
+    warps_dict: dict[tuple[int, int], np.ndarray] = {}
+    for chain in chains.values():
+        for junction in chain:
+            if junction not in warps_dict:
+                src_idx, dst_idx = junction
+                matches = matches_dict[junction]
+                P, mask = cv2.findHomography(
+                    np.array(
+                        [kps_descs_dict[src_idx][0][m.queryIdx].pt for m in matches]
+                    ).reshape(-1, 2),
+                    np.array(
+                        [kps_descs_dict[dst_idx][0][m.trainIdx].pt for m in matches]
+                    ).reshape(-1, 2),
+                    cv2.RANSAC,
+                )
+
+                # if dst_idx == center_idx:
+                #     P[0, 2] += x_offset
+                #     P[1, 2] += y_offset
+
+                warps_dict[junction] = P
+                print(f"P{src_idx=},{dst_idx=}=\n{P}")
+    return warps_dict
+
+
+def stitch_images(
+    chains: dict[int, list[tuple[int, int]]],
+    imgs: dict[int, cv2.typing.MatLike],
+    CANVAS_HEIGHT: int,
+    CANVAS_WIDTH: int,
+    canvas: cv2.typing.MatLike,
+    warps_dict: dict[tuple[int, int], np.ndarray],
+):
+    for img_idx, chain in chains.items():
+        img = imgs[img_idx]
+        for junction in chain:
+            P = warps_dict[junction]
+            img = cv2.warpPerspective(
+                src=img,
+                M=P,
+                # 在OpenCV中，图像size一般是先指定宽，后指定高
+                dsize=(CANVAS_WIDTH, CANVAS_HEIGHT),
+                dst=None,
+                borderMode=cv2.BORDER_CONSTANT,
+                borderValue=(0, 0, 0),
+            )
+        mask = (img > 0).astype(np.uint8) * 255
+        # cv2.imshow(f"mask: {img_idx=}", cv2.resize(mask, (1500, 1000)))
+
+        # Code below will give awful result, it's not correct.
+        # canvas = cv2.seamlessClone(img, canvas, mask, (CANVAS_WIDTH // 2, CANVAS_HEIGHT // 2), cv2.NORMAL_CLONE)
+        # canvas = cv2.copyTo(img, mask, canvas)
+        cv2.copyTo(img, mask, canvas)
--- main.py
+++ main.py
@@ -2,132 +2,64 @@ import cv2
 import numpy as np
 
 from utils import build_chains2, get_instructions2
-from cvoperations import center_images
+from cvoperations import (
+    center_images,
+    get_kps_descs_dict,
+    get_matches_dict,
+    get_warps_dict,
+    stitch_images
+)
 
-# 读取关于如何拼这个图的指引
-center, chains = get_instructions2("./instructions.json")
-chains = build_chains2(center, chains)
-center_idx = int(center.split(".")[0])
+def main():
 
-imgs = [str(key) + ".JPG" for key in chains.keys()]
-imgs.append(center)
-# imgs = os.listdir("./images/imgs_lesser")
+    # Read the instructions on how to stitch the images.
+    center, chains = get_instructions2("./instructions.json")
+    chains = build_chains2(center, chains)
+    center_idx = int(center.split(".")[0])
 
-detector = cv2.SIFT.create(contrastThreshold=0.07, edgeThreshold=2.5)
-# detector = cv2.SIFT.create()
-matcher = cv2.BFMatcher.create(crossCheck=True)
+    imgs = [str(key) + ".JPG" for key in chains.keys()]
+    imgs.append(center)
 
-imgs = dict(
+    detector = cv2.SIFT.create(contrastThreshold=0.07, edgeThreshold=2.5)
+    matcher = cv2.BFMatcher.create(crossCheck=True)
+
+    imgs = dict(
     [
         (int(img.split(".")[0]), cv2.imread("./images/imgs_lesser/" + img))
         for img in imgs
     ]
 )
 
-IMAGE_HEIGHT = imgs[center_idx].shape[0]
-IMAGE_WIDTH = imgs[center_idx].shape[1]
+    IMAGE_HEIGHT = imgs[center_idx].shape[0]
+    IMAGE_WIDTH = imgs[center_idx].shape[1]
 
-# 扩展画布
-CANVAS_HEIGHT, CANVAS_WIDTH = 4000, 6000
+    # Enlarge the canvas
+    CANVAS_HEIGHT, CANVAS_WIDTH = 4000, 6000
 
-x_offset = (CANVAS_WIDTH - IMAGE_WIDTH) // 2
-y_offset = (CANVAS_HEIGHT - IMAGE_HEIGHT) // 2
-print(f"{x_offset=},{y_offset=}")
+    x_offset = (CANVAS_WIDTH - IMAGE_WIDTH) // 2
+    y_offset = (CANVAS_HEIGHT - IMAGE_HEIGHT) // 2
+    print(f"{x_offset=},{y_offset=}")
 
-canvas = np.zeros((CANVAS_HEIGHT, CANVAS_WIDTH, 3), dtype=np.uint8)
+    canvas = np.zeros((CANVAS_HEIGHT, CANVAS_WIDTH, 3), dtype=np.uint8)
 
-# cv2.imshow("canvas", cv2.resize(canvas, (1500, 1000)))
-# cv2.waitKey(0)
+    # Center all the images to the larger canvas
+    center_images(imgs, IMAGE_HEIGHT, IMAGE_WIDTH, x_offset, y_offset, canvas)
 
-# Center all the images to the larger canvas
-center_images(imgs, IMAGE_HEIGHT, IMAGE_WIDTH, x_offset, y_offset, canvas)
+    # Calculate keypoints and get descriptors for each keypoint.
+    kps_descs_dict = get_kps_descs_dict(imgs, detector)
 
-# cv2.imshow("Canvas", cv2.resize(canvas, (1500, 1000)))
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+    # Match all keypoints through the perspective chain.
+    matches_dict = get_matches_dict(chains, kps_descs_dict, matcher)
 
-# 计算关键点
-kps_descs_dict: dict[int, tuple[tuple[cv2.KeyPoint], np.ndarray]] = {}
-for img_idx, img in imgs.items():
-    if img_idx not in kps_descs_dict:
-        kps_descs_dict[img_idx] = detector.detectAndCompute(img, None)
+    # Get all perspective transforming matrix
+    warps_dict = get_warps_dict(chains, kps_descs_dict, matches_dict)
 
-# 在透视链上进行关键点匹配，得到所有需要的DMatch对象
-matches_dict: dict[tuple[int, int], tuple[cv2.DMatch]] = {}
-for chain in chains.values():
-    for junction in chain:
-        if junction not in matches_dict:
-            src_idx, dst_idx = junction
-            # 注意是对所有描述符进行匹配，也就是上面标注的np.ndarray
-            matches_dict[junction] = matcher.match(
-                # 尤其注意这一行，是从query到train的匹配
-                kps_descs_dict[src_idx][1],
-                kps_descs_dict[dst_idx][1],
-            )
-            out = np.zeros((IMAGE_HEIGHT, IMAGE_WIDTH * 2, 3))
-            kps1 = kps_descs_dict[src_idx][0]
-            kps2 = kps_descs_dict[dst_idx][0]
-            matches = matches_dict[junction]
-            out = cv2.drawMatches(
-                imgs[src_idx], kps1, imgs[dst_idx], kps2, matches, out, 10
-            )
-            # plt.imshow(out[:,:,::-1])
-            # print(f"matches:{src_idx=},{dst_idx=}")
-            # plt.show()
-            # cv2.imshow(f"matches:{src_idx=},{dst_idx=}", out)
-            # cv2.waitKey(0)
-            # cv2.destroyAllWindows()
+    # And stitch them all together
+    stitch_images(chains, imgs, CANVAS_HEIGHT, CANVAS_WIDTH, canvas, warps_dict)
 
-# 构建所有需要的单应性矩阵
-warps_dict: dict[tuple[int, int], np.ndarray] = {}
-for chain in chains.values():
-    for junction in chain:
-        if junction not in warps_dict:
-            src_idx, dst_idx = junction
-            matches = matches_dict[junction]
-            P, mask = cv2.findHomography(
-                np.array(
-                    [kps_descs_dict[src_idx][0][m.queryIdx].pt for m in matches]
-                ).reshape(-1, 2),
-                np.array(
-                    [kps_descs_dict[dst_idx][0][m.trainIdx].pt for m in matches]
-                ).reshape(-1, 2),
-                cv2.RANSAC,
-            )
+    cv2.imshow("Canvas", cv2.resize(canvas, (1500, 1000)))
+    cv2.waitKey(0)
+    cv2.destroyWindow("Canvas")
 
-            # if dst_idx == center_idx:
-            #     P[0, 2] += x_offset
-            #     P[1, 2] += y_offset
-
-            warps_dict[junction] = P
-            print(f"P{src_idx=},{dst_idx=}=\n{P}")
-
-# 开拼
-for img_idx, chain in chains.items():
-    img = imgs[img_idx]
-    for junction in chain:
-        P = warps_dict[junction]
-        img = cv2.warpPerspective(
-            src=img,
-            M=P,
-            # 在OpenCV中，图像size一般是先指定宽，后指定高
-            dsize=(CANVAS_WIDTH, CANVAS_HEIGHT),
-            dst=None,
-            borderMode=cv2.BORDER_CONSTANT,
-            borderValue=(0, 0, 0),
-        )
-    mask = (img > 0).astype(np.uint8) * 255
-    # cv2.imshow(f"mask: {img_idx=}", cv2.resize(mask, (1500, 1000)))
-
-    # Code below will give awful result, it's not correct.
-    # canvas = cv2.seamlessClone(img, canvas, mask, (CANVAS_WIDTH // 2, CANVAS_HEIGHT // 2), cv2.NORMAL_CLONE)
-    canvas = cv2.copyTo(img, mask, canvas)
-
-    # cv2.imshow(f"{img_idx=}", cv2.resize(img, (1500, 1000)))
-    # cv2.waitKey(0)
-    # cv2.destroyWindow(f"{img_idx=}")
-    # plt.imshow(img[:,:,::-1])
-    # plt.show()
-cv2.imshow("Canvas", cv2.resize(canvas, (1500, 1000)))
-cv2.waitKey(0)
-cv2.destroyWindow("Canvas")
+if __name__ == "__main__":
+    main()
