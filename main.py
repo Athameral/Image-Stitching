a604c86032545104e810d4116a933f16d5dc5821
Athameral <Athameral@outlook.com>
2024-06-25 18:28:37 +0800

    Add argparser.

--- main.py
+++ main.py
@@ -4,9 +4,21 @@ import argparse
 
 from stitcher import Stitcher
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--contrastThreshold", type=float, default=0.07)
+parser.add_argument("--edgeThreshold", type=float, default=2.5)
+parser.add_argument("instructions", type=str, help="path to instructions.json")
+parser.add_argument("imagesFolder", type=str, help="path to the images folder")
+
+args = parser.parse_args().__dict__
 
 def main():
-    stitcher = Stitcher(imageFolderPath="./images/imgs_lesser")
+    stitcher = Stitcher(
+        contrastThreshold=args["contrastThreshold"],
+        edgeThreshold=args["edgeThreshold"],
+        instructionPath=["instructions"],
+        imageFolderPath=args["imagesFolder"],
+    )
     stitcher.stitch()
 
     cv2.imwrite("./result.jpg", stitcher.canvas, [cv2.IMWRITE_JPEG_QUALITY, 90])
